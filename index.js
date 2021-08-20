7783cc21e9a9473f205305a17a4d1c37a4c42782
Olatomide Afolabi <olatomideafolabi@gmail.com>
2021-08-19 21:23:39 -0800

    stage2 task

---
+++ index.js
@@ -0,0 +1,9 @@
+let showMe = document.getElementById("name-switch");
+
+let me = document.getElementById("my-name");
+
+
+showMe.addEventListener ('click', ()=>{
+     me.innerHTML = "Olatomide Afolabi";
+});
+
